17f970d68bac8df4cb6051029cfc76bac861635a
Jaz wu <592015984b@gmail.com>
2017-04-24 17:09:45 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i 's/xx@xx.xx/'$email'/g' /var/www/html/api.php
+sed -i 's/user/'$user'/g' /var/www/html/api.php
 sed -i 's/passwd/'$passwd'/g' /var/www/html/api.php
 
 /bin/bash
