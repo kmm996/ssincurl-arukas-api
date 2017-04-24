5ae8780137eca11806a59ce18f35d62eb4459df2
Jaz wu <592015984b@gmail.com>
2017-04-24 15:35:52 +0800

    Add files via upload

---
+++ api.php
@@ -0,0 +1,67 @@
+<?php
+
+
+$cookie = dirname(__FILE__) . '/cookie_ss.txt';
+
+//登录成功之后保存cookie
+login($cookie);
+
+//获取数据
+get_content($cookie);
+
+//删除cookie文件
+@ unlink($cookie);
+
+
+function login($cookie)
+{
+    $data = array(
+        'email' => "email", //此处输入您的arukas登录邮箱
+        'password' => "password" //此处输入您的arukas登录密码
+    );
+    $loginUrl = "https://app.arukas.io/api/login";
+    $ch_login = curl_init();
+    curl_setopt($ch_login, CURLOPT_URL, $loginUrl);
+    curl_setopt($ch_login, CURLOPT_POST, 1);
+    curl_setopt($ch_login, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch_login, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch_login, CURLOPT_COOKIEJAR, $cookie);
+    $result = curl_exec($ch_login);
+
+    $content = json_decode($result);
+
+    $status = $content->status;
+
+    if ($status != "200") {
+        echo $content->message;
+        echo ' Please check the api.php file.';
+        exit;
+    };
+    curl_close($ch_login);
+}
+
+
+//登录成功后获取数据
+function get_content($cookie)
+{
+    //登录成功之后访问的页面
+    $contextUrl = "https://app.arukas.io/api/containers";
+    $ch_content = curl_init();
+    curl_setopt($ch_content, CURLOPT_URL, $contextUrl);
+    //curl_setopt($ch, CURLOPT_HEADER, 0);
+    //curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+    curl_setopt($ch_content, CURLOPT_COOKIEFILE, $cookie); //读取cookie
+    $result = curl_exec($ch_content);
+
+    $httpcode = curl_getinfo($ch_content, CURLINFO_HTTP_CODE);
+    if ($httpcode != "200") {
+        echo "Oops!";
+        exit;
+    }
+
+    return $result;
+    curl_close($ch_content);
+}
+
+
+?>
---
+++ js/app.js
@@ -0,0 +1,91 @@
+new Vue({
+    el: '#app',
+    data: {
+        email: '',
+        password: '',
+        configs: null,
+        //configsCache:localStorage.configsCache
+    },
+    methods: {
+        newConfig: function () {
+            layer.msg('Just a moment...', {
+                time: 0, //不自动关闭
+                shadeClose: false,
+                shade: 0.3
+            });
+
+            this.$http.get('api.php?email=' + this.email + '&password=' + this.password, {emulateJSON: true}).then(
+                function (response) {
+                    if (response.body.indexOf('Failed') != -1) {
+                        layer.msg(response.body, {icon: 2});
+                        return
+                    }
+                    var result = JSON.parse(response.body)
+                    if (result.data.length<1) {
+                        layer.msg('Failed !');
+                        return
+                    }
+                    this.configs = []
+                    for (var i = 0; i < result.data.length; i++) {
+                        var mappingsObj = result.data[i].attributes.port_mappings
+                        var cmd = result.data[i].attributes.cmd
+                        var pwd = this.rePwd(cmd)
+                        var lock = this.reLock(cmd)
+                        var image_name = result.data[i].attributes.image_name
+                        if (image_name.indexOf("ss-with-net-speeder") ==-1) {
+                            continue
+                        }
+                        var mappingText = JSON.stringify(mappingsObj)
+                        var mappingJson = eval('(' + mappingText + ')')
+                        var ssHead = 'ss://'
+                        if (mappingJson) {
+                            for (var j = 0; j < mappingJson.length; j++) {
+                                for (var k = 0; k < mappingJson[j].length; k++) {
+                                    var mappingResult = {}
+                                    mappingResult.service_port = mappingJson[j][k].service_port
+                                    mappingResult.container_port = mappingJson[j][k].container_port
+                                    mappingResult.host = this.reHost(mappingJson[j][k].host)
+                                    mappingResult.cmd = cmd
+                                    mappingResult.pwd = pwd
+                                    mappingResult.lock = lock
+                                    ssUrl = lock+':'+pwd+'@'+this.reHost(mappingJson[j][k].host)+':'+mappingJson[j][k].service_port
+                                    mappingResult.ss_url = ssHead+this.base64DeCode(ssUrl)
+                                    this.configs.push(mappingResult)
+                                }
+                            }
+                        }
+                    }
+                    layer.msg('Successful !')
+                }
+            )
+        },
+        clearForm: function () {
+            this.configs = null
+        },
+        reHost: function (oldhost) {
+            host = oldhost.match(/seaof-(\S*).jp/)[1]
+            host = host.replace(/-/g, ".")
+            return host
+        },
+        rePwd: function (oldpwd) {
+            if(oldpwd == null) return
+            pwd = oldpwd.substring(oldpwd.indexOf("k ") + 2,oldpwd.indexOf(" -m"))
+            return pwd
+        },
+        reLock: function (oldLock) {
+            if(oldLock == null) return
+            lock = oldLock.substring(oldLock.indexOf("m ")+2)
+            return lock
+        },
+        base64DeCode: function (str) {
+            var rawStr = str
+            var wordArray = CryptoJS.enc.Utf8.parse(rawStr)
+            var base64 = CryptoJS.enc.Base64.stringify(wordArray)
+            return base64
+        },
+        showQrCode: function (url,port) {
+            $("canvas").remove()
+            $("#"+port).qrcode(url)
+        },
+    }
+})
